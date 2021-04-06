abd8422bf738353623bc612c975405ca5a01f1a9
BruttherJOE <bruttherjoe@gmail.com>
2021-04-06 17:45:57 +0800

    Add files via upload

---
+++ Linux,Pi Notes/joe_installer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "This is BRUTTHERJOE'S custom Install script"
+echo "Packages that will be installed : 
+python-pip 
+python3-pip"
+
+
+
+
+# Install Packages
+PACKAGES=python-pip python3-pip"
+apt-get update
+apt-get upgrade -y
+apt-get install $PACKAGES -y
+
+
+
+
+
+
+
+# END SEQ
+echo "Installation complete."
